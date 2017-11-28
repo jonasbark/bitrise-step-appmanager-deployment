834e990bf19465bfe5bfae1772f218423de8eba0
Jonas Bark <jonas.bark@ffuf.de>
2017-11-28 12:44:35 +0100

    setup

---
+++ step.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -ex
+
+/bin/echo -n "Uploading ${IPA_FILENAME} as ${IOS_APP_VERSION_NAME} to AppManager... "
+JSON=$( curl -s $appManUrl$appId -F api_key=$api_key -F iosIpaFile="${BITRISE_IPA_PATH}" -F addedIosVersion="${IOS_APP_VERSION_NAME}" -A "App Manager Upload" )
+/bin/echo -n JSON
+
+exit 0
