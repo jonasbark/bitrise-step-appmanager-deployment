7f1db62f3f5db33999855c976dad3442be97d1a5
Jonas Bark <jonas.bark@ffuf.de>
2017-11-28 14:41:13 +0100

    fix file upload name

--- step.sh
+++ step.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -ex
 
-/bin/echo -n "Uploading ${IPA_FILENAME} as ${IOS_APP_VERSION_NAME} to AppManager... "
-JSON=$( curl -s $appManUrl$appId -F api_key=$api_key -F iosIpaFile="$ipaPath" -F addedIosVersion="${IOS_APP_VERSION_NAME}" -A "App Manager Upload" )
+/bin/echo -n "Uploading ${IPA_FILENAME} as ${IOS_APP_VERSION_CODE} to AppManager... "
+JSON=$( curl -s $appManUrl$appId -F api_key=$api_key -F iosIpaFile="@$ipaPath" -F addedIosVersion="${IOS_APP_VERSION_CODE}" -A "App Manager Upload" )
 /bin/echo -n JSON
 
 exit 0
